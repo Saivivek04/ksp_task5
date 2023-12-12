a9a88ab8c605b2c94341bde8425e3ec440766be2
Sai Vivek <132122375+Saivivek04@users.noreply.github.com>
2023-12-12 19:44:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+console.log("Hello World");
+alert("Welcome");
+
+let box1="2";
+let box2="3";
+let box3="10";
+console.log(box3);
+console.log("box3");
+console.log(box1);
+
+let num1="sai";
+let num2="200";
+let num3="300";
+console.log(num1 + num2 + num3);
+
+let word="Car";
+console.log(word);
+
+let letter='A';
+console.log(letter);
+
+let num=10;
+num=20;
+console.log(num);
+
+const n=10;
+//n=20;
+console.log(n);
+
+//Arthimetic Operators...
+let A=5;
+let B=2;
+console.log(A+B);
+console.log(A-B);
+console.log(A*B);
+console.log(A/B);
+console.log(A%B);
+console.log(A**B);
+//Arthimetic Operators Pratice.
+let C=10;
+C++;
+console.log(C);//11
+
+let D=10;
+console.log(D++);//10
+
+let E=10;
+console.log(++E);//11
+
+let F=10;
+result=F++;
+console.log(F);//11
+
+let G=10;
+G--;
+console.log(G);
+
+//Assignment opertors Pratice.
+let H=10;
+H+=5;
+console.log(H);
+
+let I=10;
+I=I+5;
+console.log(I);
+
+let J=2;
+J**=3;
+console.log(J);
+
+let K=2;
+K=K**3;
+console.log(K);
+
+let L=10;
+console.log(L==20);//False
+
+let m=20;
+let p="20";
+console.log(m==p);//True
+
+let o=20;
+let q="20";
+console.log(o===q);//False
+
+let r=10;
+let s=20;
+console.log(r!=s);//True
+console.log(r>s);//False
